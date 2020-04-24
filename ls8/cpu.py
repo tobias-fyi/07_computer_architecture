b27c244ff3e1eee696cd6b70133781937734c53a
tobias-fyi <tobyreaper@gmail.com>
2020-04-24 10:46:17 -0600

    Computer architecture SC - first-pass and one stretch

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,14 +10,16 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         # 256 bytes (8 bits = 1 byte) of RAM
-        self.ram = [0] * 256
+        self.ram = [0b00000000] * 256
 
         # === Internal registers === #
-        self.reg = [0] * 8  # 8 general-purpose registers
-        self.sp = 7  # R7 is reserved as the Stack Pointer (SP)
+        self.reg = [0b00000000] * 0b00001000  # 8 general-purpose registers
+        self.sp = 0b00000111  # R7 is reserved as the Stack Pointer (SP)
         self.reg[self.sp] = 0xF4  # Stack starts at memory address F4
         # PC: address (index) of currently executing instruction
-        self.pc = 0
+        self.pc = 0b00000000
+        # FL: flags - used in CMP opcode
+        self.fl = 0b00000000
 
         # Attribute to control runtime
         self.running = False
@@ -34,6 +36,9 @@ class CPU:
             0b01000110: self.pop,
             0b01010000: self.call,
             0b00010001: self.ret,
+            0b01010100: self.jmp,
+            0b01010101: self.jeq,
+            0b01010110: self.jne,
         }
 
     def load(self, program_filepath: str) -> None:
@@ -59,7 +64,7 @@ class CPU:
                 inst = inst[:cut_index]  # Slice from index to end
             inst = int(inst, 2)  # Convert to bit
             self.ram[address] = inst  # Assign value to memory address
-            address += 1  # Move to next memory slot
+            address += 0b00000001  # Move to next memory slot
 
     def ram_read(self, address) -> int:
         """Returns the value (MDR) stored at a memory address (MAR)."""
@@ -87,7 +92,7 @@ class CPU:
         given register onto the stack.
         """
         # Decrement stack pointer
-        self.reg[self.sp] -= 1
+        self.reg[self.sp] -= 0b00000001
         # Copy value to be pushed to stack from reg to ram
         value = self.reg[self.operands[0]]
         address = self.reg[self.sp]  # Get address from R7
@@ -104,15 +109,15 @@ class CPU:
         # Assign value to the given register
         self.reg[self.operands[0]] = value
         # Increment the stack pointer
-        self.reg[self.sp] += 1
+        self.reg[self.sp] += 0b00000001
 
     def call(self):
         """CALL: Calls a subroutine (function) at the
         address stored in the register."""
         # Get return address (PC + 2)
-        self.reg[self.sp] -= 1  # Decrement stack pointer
+        self.reg[self.sp] -= 0b00000001  # Decrement stack pointer
         # Push return address -> stack
-        self.ram[self.reg[self.sp]] = self.pc + 2
+        self.ram[self.reg[self.sp]] = self.pc + 0b00000010
 
         # Set PC to value in given register (dest address)
         dest_addr = self.reg[self.operands[0]]
@@ -122,9 +127,31 @@ class CPU:
         """RET: Return from subroutine to main scope."""
         # Pop return address from stack
         return_addr = self.ram[self.reg[self.sp]]
-        self.reg[self.sp] += 1  # Increment stack pointer
+        self.reg[self.sp] += 0b00000001  # Increment stack pointer
         # Set PC to return address
         self.pc = return_addr
+
+    def jmp(self):
+        """JMP: Jump to the address stored in the given register."""
+        # Set PC to address stored in register
+        self.pc = self.reg[self.operands[0]]
+        # self.pc = self.operands[0]
+
+    def jeq(self):
+        """If `equal` flag is set (true), jump to
+        the address stored in the given register."""
+        if self.fl == 1:
+            self.jmp()
+        else:
+            self.pc += 2
+
+    def jne(self):
+        """If `E` flag is clear (false, 0), jump to
+        the address stored in the given register."""
+        if self.fl == 0:
+            self.jmp()
+        else:
+            self.pc += 2
 
     def run(self):
         """Run the CPU."""
@@ -148,11 +175,8 @@ class CPU:
             # Look up / execute the current instruction
             if ir in self.opcodes:
                 self.opcodes[ir]()
-            # TODO: figure out how to add ALU to lookup
-            elif ir == 0b10100010:  # MUL: Multiply
-                self.alu("MUL")
-            elif ir == 0b10100000:  # ADD
-                self.alu("ADD")
+            elif alu_op == 1:  # ALU instructions
+                self.alu(ir)
             else:  # Catch invalid / other instruction
                 print("Unrecognized instruction")
                 self.running = False
@@ -165,10 +189,42 @@ class CPU:
         """ALU operations."""
         reg_a, reg_b = self.operands  # Extract operands from array
 
-        if op == "ADD":
+        if op == 0b10100000:  # ADD
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == "MUL":
+        elif op == 0b10101000:  # AND
+            # Bitwise-AND then store result at reg_a
+            self.reg[reg_a] &= self.reg[reg_b]
+        elif op == 0b10100111:  # CMP
+            # Set self.fl to 1 if equal, 0 if not
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.fl = 0b00000001
+            else:
+                self.fl = 0b00000000
+        elif op == 0b10100100:  # MOD
+            # Get modulo of values at the two regs
+            mod = self.reg[reg_a] % self.reg[reg_b]
+            if mod > 0:
+                # If there is any remainder, store at reg_a
+                self.reg[reg_a] = mod
+            else:  # Otherwise, print error and HLT
+                print("Modulo result is 0. Quitting...")
+                self.hlt()
+        elif op == 0b10100010:  # MUL
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == 0b01101001:  # NOT
+            self.reg[reg_a] = ~self.reg[reg_a]
+        elif op == 0b10101010:  # OR
+            # Bitwise-OR then store result at reg_a
+            self.reg[reg_a] |= self.reg[reg_b]
+        elif op == 0b10101100:  # SHL
+            # Shift val at reg_a left by val of reg_b
+            self.reg[reg_a] << self.reg[reg_b]
+        elif op == 0b10101101:  # SHR
+            # Shift val at reg_a left by val of reg_b
+            self.reg[reg_a] >> self.reg[reg_b]
+        elif op == 0b10101011:  # XOR
+            # Bitwise-XOR, store result at reg_a
+            self.reg[reg_a] ^= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -202,7 +258,7 @@ if __name__ == "__main__":
     # === Debugging setup === #
     to_repo = "/Users/Tobias/workshop/buildbox/lambda_cs"
     to_dir = "07_computer_architecture/ls8/examples/"
-    to_file = "mult.ls8"
+    to_file = "sctest.ls8"
     filepath = os.path.join(to_repo, to_dir, to_file)
 
     cpu = CPU()
